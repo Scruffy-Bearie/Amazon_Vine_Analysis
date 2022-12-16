fcd7463e80bd64893ebe8141f6cd4a03f8338059
Sean_Milligan <rseanmilligan@hotmail.com>
2022-12-16 07:56:51 -0800

    coding, images and data

---
+++ CODING/VIne_Reviews_RAnalysis.R
@@ -0,0 +1,20 @@
+# USE THE LIBRARY FUNCTION TO LOAD REQUIRED PACKAGES
+library(dplyr)
+library(tidyverse)
+
+# IMPORT AND READ REQUIRED CSV FILE AS DATAFRAME
+vine_reviews <- read.csv('DATA/vine_reviews.csv')
+head(vine_reviews)
+
+# OBTAIN THE MEAN, MEDIAN, VARIANCE AND STANDARD DEVIATION OF THE DATAFRAMES "star_rating" COLUMN
+total_summary <- vine_reviews %>% summarize(Mean=mean(star_rating), Median=median(star_rating), Variance=var(star_rating), SD=sd(star_rating))
+print(total_summary)
+
+
+# OBTAIN THE MEAN, MEDIAN, VARIANCE AND STANDARD DEVIATION OF THE DATAFRAMES "star_rating" COLUMN GROUPED BY VINE MEMBERSHIP
+vine_summary <- vine_reviews %>% group_by(vine) %>% summarize(Mean=mean(star_rating), Median=median(star_rating), Variance=var(star_rating), SD=sd(star_rating), .groups = 'keep')
+print(vine_summary)
+
+# CONDUCT TWO-SAMPLE T-TEST TO COMPARE PAID RATINGS TO UNPAID RATINGS
+t.test(subset(vine_reviews, vine == "Y")$star_rating, subset(vine_reviews, vine == "N")$star_rating)
+
