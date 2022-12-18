24fb5441d0764579c53c710b6ae16a5049b5b24e
Sean Milligan <111652332+Scruffy-Bearie@users.noreply.github.com>
2022-12-18 07:05:12 -0800

    Add files via upload

--- CODING/VIne_Reviews_RAnalysis.R
+++ CODING/VIne_Reviews_RAnalysis.R
@@ -6,11 +6,6 @@ library(tidyverse)
 vine_reviews <- read.csv('DATA/vine_reviews.csv')
 head(vine_reviews)
 
-# OBTAIN THE MEAN, MEDIAN, VARIANCE AND STANDARD DEVIATION OF THE DATAFRAMES "star_rating" COLUMN
-total_summary <- vine_reviews %>% summarize(Mean=mean(star_rating), Median=median(star_rating), Variance=var(star_rating), SD=sd(star_rating))
-print(total_summary)
-
-
 # OBTAIN THE MEAN, MEDIAN, VARIANCE AND STANDARD DEVIATION OF THE DATAFRAMES "star_rating" COLUMN GROUPED BY VINE MEMBERSHIP
 vine_summary <- vine_reviews %>% group_by(vine) %>% summarize(Mean=mean(star_rating), Median=median(star_rating), Variance=var(star_rating), SD=sd(star_rating), .groups = 'keep')
 print(vine_summary)
@@ -18,3 +13,6 @@ print(vine_summary)
 # CONDUCT TWO-SAMPLE T-TEST TO COMPARE PAID RATINGS TO UNPAID RATINGS
 t.test(subset(vine_reviews, vine == "Y")$star_rating, subset(vine_reviews, vine == "N")$star_rating)
 
+# USE THE MANN-WHITNEY AND KRUSKAL WALLACE TESTS TO COMPARE POPULATIONS
+wilcox.test(star_rating ~ vine, data = vine_reviews)
+kruskal.test(star_rating ~ vine, data = vine_reviews)
